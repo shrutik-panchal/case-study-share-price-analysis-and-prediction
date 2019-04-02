78ee7361afeac955f4019f943f5ef0c3cba69e17
Shrutik Panchal <sypanchal@deakin.edu.au>
2019-04-02 18:56:11 +1100

    New and Update: Trends and comments
    Added Logic for Trends as need to include Volume data with colors and updated some comments

--- Real-world-analytics-A2.R
+++ Real-world-analytics-A2.R
@@ -13,16 +13,25 @@ Telstra_Hist_data <- read.csv(file="AGL.AX.csv", header = TRUE, sep=",")
 Odata <- Telstra_Hist_data
 Odata$Date <- as.Date(Odata$Date, format="%d/%m/%Y")
 
+# Creating column for Trends - Increasing and Decreasing
+for (i in 1:length(Odata[,1])){
+  if (Odata$Close[i] >= Odata$Open[i]){Odata$Direction[i] = "Increasing"}
+  else {Odata$Direction[i] = "Decreasing"}
+}
+
 # Creating color lists for Bullish and Bearish Candle
 i <- list(line = list(color = 'green'))
 d <- list(line = list(color = 'pink'))
 
 # Creating basic candle stick diagram to visualize share price
 chart <- Odata %>%
+  # Creating Plot and assigning appropriate columns of data
   plot_ly(x = ~Date, type = "candlestick",
           open = ~Open, close = ~Close,
           high = ~High, low = ~Low,
           increasing = i, decreasing = d) %>%
-  layout(title = "Financial Data",
-         xaxis = list(rangeslider = list(visible = F)))
+  # Layout Creation
+  layout(title = "AGL Energy Limited Share Price: Behaviour and Performance (Mar-May 2018)",
+         xaxis = list(rangeslider = list(visible = F), title = "Date"),
+         yaxis = list(title = "Share Price"))
 chart
